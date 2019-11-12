57593a0e81d1b31b74ec9e1abfc679a19a3850d6
ReadySetJoe <wolfgangwallaceband@gmail.com>
2019-11-12 16:09:23 -0500

    heroku debugging 1

--- conf/settings.py
+++ conf/settings.py
@@ -168,8 +168,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # UPLOADED FILE CONFIGURATION
 #  SEE: https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+# AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+# AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = 'ccs-drf-react-recipes-readysetjoe'
 AWS_DEFAULT_ACL = None
 # # default will be to lock down
